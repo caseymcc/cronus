ad139b42db54f447ca309a2da36bf266fd68ebb1
Krazer <caseymcc@krazer.net>
2025-01-26 12:35:46 -0600

    style: Reformat terminal_ui.cpp with consistent code style

--- src/cronus/terminal_ui.cpp
+++ src/cronus/terminal_ui.cpp
@@ -10,7 +10,8 @@ namespace cronus
 
 TerminalUI::TerminalUI() : m_screen(Screen::Create(Dimension::Full(), Dimension::Fixed(1))) {}
 
-void TerminalUI::render(const Element& element) const {
+void TerminalUI::render(const Element &element) const
+{
     m_screen.Clear();
     Render(m_screen, element);
     m_screen.Print();
@@ -18,49 +19,52 @@ void TerminalUI::render(const Element& element) const {
 
 void TerminalUI::displayWelcome() const
 {
-    auto welcome = text("Cronus Client") | bold | color(Color::Blue);
+    auto welcome=text("Cronus Client")|bold|color(Color::Blue);
     render(welcome);
 }
 
 void TerminalUI::displayResponse(const std::string &provider, const std::string &response) const
 {
-    auto header = text(provider + " Response:") | bold | color(Color::Green);
-    auto content = text(response);
-    auto element = vbox({
+    auto header=text(provider+" Response:")|bold|color(Color::Green);
+    auto content=text(response);
+    auto element=vbox({
         header,
         content
-    }) | border;
+        })|border;
     render(element);
 }
 
 void TerminalUI::displayError(const std::string &message) const
 {
-    auto error = text("Error: " + message) | bold | color(Color::Red);
-    render(error | border);
+    auto error=text("Error: "+message)|bold|color(Color::Red);
+    render(error|border);
 }
 
 std::string TerminalUI::getUserInput() const
 {
     std::string input;
-    auto screen = ScreenInteractive::TerminalOutput();
-    
-    Component inputBox = Input(&input, "Enter your message");
-    auto renderer = Renderer(inputBox, [&] {
-        return vbox({
-            text("Enter your message:") | bold,
-            inputBox->Render() | border
-        });
-    });
+    auto screen=ScreenInteractive::TerminalOutput();
 
-    auto enterPressed = false;
-    inputBox |= CatchEvent([&](Event event) {
-        if (event == Event::Return) {
-            enterPressed = true;
-            screen.ExitLoopClosure()();
-            return true;
-        }
-        return false;
-    });
+    Component inputBox=Input(&input, "Enter your message");
+    auto renderer=Renderer(inputBox, [&]
+        {
+            return vbox({
+                text("Enter your message:")|bold,
+                inputBox->Render()|border
+                });
+        });
+
+    auto enterPressed=false;
+    inputBox|=CatchEvent([&](Event event)
+        {
+            if(event==Event::Return)
+            {
+                enterPressed=true;
+                screen.ExitLoopClosure()();
+                return true;
+            }
+            return false;
+        });
 
     screen.Loop(renderer);
     return input;
